151dbdab08c3306da56fe8fd0d840eca286ac9b7
Nate-Huerd <nhuerd3@gmail.com>
2022-04-23 19:45:54 -0500

    adjusted code to save inputs

--- script.js
+++ script.js
@@ -1,5 +1,72 @@
 $(document).ready(function () { 
+    // add click listener
+    $('.saveBtn').on('click', function() {
+        var time = $(this)
+            .parent()
+            .attr('id');
+        var value = $(this)
+            .siblings('description')
+            .val();
 
+        // save time and value into localStorage for retrieval later
+        localStorage.setItem(time, value);
+    });
+
+    // Add hours function
+    function hourUpdater() {
+        var currentHour = moment().hours();
+
+        // this loops us through the time blocks
+        $('.time-block').each(function() {
+            var blockHour = parseInt(
+                $(this)
+                    .attr('id')
+                    .split('-')[1]
+            );
+        
+            // here we will cycle through Past, Present, and Future
+            if (blockHour < currentHour) {
+                $(this).addClass('past');
+            } else if (blockHour === currentHour) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            } else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+        });
+    }
+
+    // call hourUpdater
+    hourUpdater();
+
+    // check to see if we need to update the current time
+    var interval = setInterval(hourUpdater, 15000);
+  
+    // call any of the saved data from localStorage
+    $('#hour-8 .description').val(localStorage.getItem('hour-8'));
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+  
+    // display current day on page
+    $('#currentDay').text(moment().format('dddd, MMMM Do'));
+  });
+
+
+
+
+
+
+
+/*
     var today = new Date();
     // Get today's date
     var day = today.getDate() + '-' + (today.getMonth() + 1) + '-' + (today.getFullYear());
@@ -101,30 +168,4 @@ $(document).ready(function () {
     document.getElementById('5pm').value = saved5pm;
 
 });
-
-
-
-/* creator notes below, ignore
-
-newish date stuff
-    // Declare today
-    var today = new Date();
-
-    // Declare current moment
-    var now = moment().format('LLLL');
-
-    // Declare currentDate and currentHour (need this to determine if a task is past, present, or future)
-    var currentDate = $('#currentDay');
-        currentDate.text(now);
-    var cal = new Date();
-    var currentHour = cal.getHours();
-
-
-Get today's day of the week
-    var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    var d = new Date();
-        let day = weekday[d.getDay()];
-            document.getElementById('currentDay').innerHTML = day;
-        let month = d.getMonth();
-        let year = d.getFullYear();
-        */
+*/
